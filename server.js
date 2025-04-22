c00143b8c9dc0d317dd28b831875d5f5dd69ae05
MBaihaqi <baihaqi1401@student.ub.ac.id>
2025-04-22 07:17:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 // Start Server - listen on all interfaces
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 }).on('error', (err) => {
